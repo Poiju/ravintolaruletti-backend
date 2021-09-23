b37d6d42c4144da62b07c2571aa26a135b4dad45
anttomi <75563171+anttomi@users.noreply.github.com>
2021-09-23 18:13:16 +0300

    Update settings.py

--- ravintolarulettibackend/settings.py
+++ ravintolarulettibackend/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-irq&6(6&tdurrp%2akl@-d^m%o=z1a6h%=sw6a&3fdk9_z^-2('
+SECRET_KEY = ############
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
