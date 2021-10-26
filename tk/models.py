5346db9d5afea135bc79f6a6438be8da8ccedc9c
ristoxxx <lahteri@gmail.com>
2021-10-26 17:41:32 +0300

    lisätty model ja pääkäyttäjä

---
+++ tk/models.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.text
