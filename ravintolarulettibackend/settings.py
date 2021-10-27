a3443f78ebecbfcf02562e7a8d41086cc1c8bfd5
ristoxxx <lahteri@gmail.com>
2021-10-27 18:16:42 +0300

    REST-alku

--- ravintolarulettibackend/settings.py
+++ ravintolarulettibackend/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tk.apps.TkConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
